5e7f5541a66c009b5083e5304a1e771bd792fee3
Paulo <eduardopeter91@gmail.com>
2025-03-11 10:59:01 -0300

    Aula 03 - Vídeo 04 - JSON

---
+++ POO/Aula_04/JSON/descricao_json.js
@@ -0,0 +1,29 @@
+/*
+JSON 
+
+• JSON = JavaScriptObjectNotation 
+• Formato textual para serialização de dados 
+• É independente de linguagem 
+• Muito utilizado para retorno de Serviços Web REST
+
+• JSON é capaz de representar: 
+• Tipos primitivos 
+• Strings, números, booleanos, null 
+• Tipos estruturados 
+• Objetos 
+• Coleção não-ordenada de zero ou mais pares chave/valor 
+• Arranjos 
+• Coleção ordenada de zero ou mais valores
+*/
+
+// Exemplo:
+
+product = {
+    "productName": "Computer Monitor",
+    "price": "229.00",
+    "specifications": {
+        "size": 22,
+        "type": "LCD",
+        "colors": ["black", "red", "white"]
+    }
+}
